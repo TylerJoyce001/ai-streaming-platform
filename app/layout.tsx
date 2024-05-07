d19e75c86e51b910089b63baa573117659e2a81a
TylerJoyce001 <tjoyc002@fiu.edu>
2024-05-07 16:02:44 -0400

    feat(header): add dark mode toggler

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/ui/Header";
+import Header from "@/components/Header";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <Header />
-        {children}</body>
+      <body className="bg-white dark:bg-[#1A1C29]">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { ThemeToggler } from "./ThemeToggler";
+
+const Header = () => {
+  return (
+    <header className="fixed w-full z-20 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
+      <Link href="/" className="mr-10">
+        <Image
+          src="https://links.papareact.com/a943ae"
+          alt="Disney Logo"
+          width={120}
+          height={100}
+          className="cursor-pointer invert-0 dark:invert"
+        />
+      </Link>
+
+      <div className="flex space-x-2">
+        {/* Genre dropdown */}
+        {/* Search Bar */}
+        <ThemeToggler />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
