a9d5a5045ea8f7565421069ce2d9378d27447483
TylerJoyce001 <tjoyc002@fiu.edu>
2024-06-17 15:02:31 -0400

    feat(genrePage): add carousels for genre page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,19 +1,30 @@
-import react from 'react';
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies, getPopularMovies } from "@/lib/getMovies";
+import react from "react";
 
 type Props = {
-    params: {
-        id: number;
-    };
+  params: {
+    id: string;
+  };
 
-    searchParams: {
-        genre: string;
-    };
+  searchParams: {
+    genre: string;
+  };
 };
 
-const GenrePage = ({params: { id }, searchParams: { genre } }: Props) => {
-    return (
-        <h1>Welcome to the genre with ID: {id} and name: {genre} </h1>
-    );
-};
+async function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
+  const movies = await getDiscoverMovies(id);
+  const popularMovies = await getPopularMovies();
 
-export default GenrePage
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+      </div>
+      <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+      <MoviesCarousel title="You may also like" movies={popularMovies} />
+    </div>
+  );
+}
+
+export default GenrePage;
