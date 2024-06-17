cba0f237eb9791f7df164d367e1ba225eab090e1
TylerJoyce001 <tjoyc002@fiu.edu>
2024-06-17 14:44:00 -0400

    feat(searchPage): add carousels for search page

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,21 +1,30 @@
-import { notFound } from 'next/navigation';
-import React from 'react';
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies";
+import { notFound } from "next/navigation";
 
 type Props = {
-    params: {
-        term: string;
-    };
+  params: {
+    term: string;
+  };
 };
 
-const SearchPage = ({ params: { term } }: Props) => {
-    if (!term) notFound();
+async function SearchPage({ params: { term } }: Props) {
+  if (!term) notFound();
 
-    const termToUse = decodeURI(term);
+  const termToUse = decodeURI(term);
 
-    // API call to get movies
-    // API call to get popular movies
-    
-    return <div>Welcome to the search page: {termToUse}</div>;
-};
+  const movies = await getSearchedMovies(termToUse);
+  const popularMovies = await getPopularMovies();
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
+        <MoviesCarousel title="Movies" movies={movies} isVertical />
+        <MoviesCarousel title="You may also like" movies={popularMovies} />
+      </div>
+    </div>
+  );
+}
 
 export default SearchPage;
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 import GenreDropdown from "./GenreDropdown";
 import SearchInput from "./SearchInput";
 
+// Need to add logo
+
 function Header() {
   return (
     <header className="fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
       <Link href="/" className="mr-10">
         <Image
-          src="https://links.papareact.com/a943ae"
-          width={120}
-          height={100}
-          alt="Disney Logo"
+          src=""
+          width={0}
+          height={0}
+          alt=""
           className={"cursor-pointer invert"}
         />
       </Link>
